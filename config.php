976b3901782181d0cf6778ce2a05a18c801333a7
Nguyễn Phương Quyên <quyenevy@Nguyens-MacBook-Pro.local>
2020-01-15 20:11:02 +0700

    Qyn Qyn dth

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+define("HOST","localhost");
+define("USER","root");
+define("PASS",'root');
+define("DB","db_banhang");
+//url goc cua project
+define("BASE_URL","hhttp://localhost:8888/php_shop-master/");
+//thu muc goc cua project
+define("ROOT",dirname(__FILE__));
+
+define("IMG_BOOK",ROOT."image/product/");
